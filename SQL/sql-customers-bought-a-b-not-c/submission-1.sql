ffca557d9d1781ff162bc78da21ae6aa01709599
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-04 16:51:10 +0530

    Add: sql-customers-bought-a-b-not-c - submission-1

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-1.sql
@@ -0,0 +1,26 @@
+-- Write your query below
+-- SELECT c.customer_id, c.customer_name 
+-- FROM customers c
+-- JOIN orders o ON c.customer_id = o.customer_id
+-- GROUP BY c.customer_id, c.customer_name
+-- HAVING SUM(CASE WHEN o.pr)
+-- ORDER BY c.customer_name;
+
+SELECT c.customer_id, c.customer_name 
+FROM customers c
+WHERE c.customer_id IN (
+    SELECT customer_id
+    FROM orders
+    WHERE product_name = 'A'
+)
+AND c.customer_id IN (
+    SELECT customer_id
+    FROM orders
+    WHERE product_name = 'B'
+)
+AND c.customer_id NOT IN (
+    SELECT customer_id
+    FROM orders
+    WHERE product_name = 'C'
+)
+ORDER BY c.customer_name ASC;
