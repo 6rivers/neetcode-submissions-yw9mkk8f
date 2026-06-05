7f98d075ff3d0dbbf315f1ef2b7eedfbf04d02e7
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-05 14:54:47 +0530

    Add: sql-customer-placing-the-largest-number-of-orders - submission-1

---
+++ SQL/sql-customer-placing-the-largest-number-of-orders/submission-1.sql
@@ -0,0 +1,18 @@
+-- Write your query below
+-- WITH order_counts AS (
+--     SELECT customer_number, COUNT(order_number) as total_orders
+--     FROM orders
+--     GROUP BY customer_number
+-- )
+
+-- SELECT customer_number
+-- FROM order_counts
+-- ORDER BY total_orders DESC
+-- LIMIT 1;
+
+SELECT customer_number
+FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
+
