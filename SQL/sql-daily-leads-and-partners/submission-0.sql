592cdd60a0b7bbb36f116c97f2f67d34f8f6a400
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-08 15:42:24 +0530

    Add: sql-daily-leads-and-partners - submission-0

---
+++ SQL/sql-daily-leads-and-partners/submission-0.sql
@@ -0,0 +1,4 @@
+-- Write your query below
+SELECT date_id, make_name, count(distinct lead_id) AS unique_leads, count(distinct partner_id) AS unique_partners
+FROM daily_sales
+GROUP BY date_id, make_name;
