1b46b9701fe482fe2e0f919f3b2a797e14a15eca
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-10 14:49:21 +0530

    Add: sql-apples-and-oranges - submission-1

---
+++ SQL/sql-apples-and-oranges/submission-1.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+SELECT sale_date, diff
+FROM (
+SELECT sale_date, fruit, sold_num -
+    LEAD(sold_num, 1, 0) OVER(PARTITION BY sale_date ORDER BY fruit) AS diff
+FROM sales)
+WHERE fruit = 'apples'
