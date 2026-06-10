d62afdb6d5341abdddad178881e189cfb365cdf3
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-10 16:20:10 +0530

    Add: sql-number-of-calls-between-two-persons - submission-0

---
+++ SQL/sql-number-of-calls-between-two-persons/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT person1, person2, COUNT(*) AS call_count, SUM(duration) as total_duration
+FROM (SELECT 
+    CASE WHEN from_id < to_id THEN from_id 
+    ELSE to_id END AS person1,
+    CASE WHEN to_id > from_id THEN to_id 
+    ELSE from_id END AS person2, 
+    duration
+FROM calls)
+GROUP BY person1, person2
+
