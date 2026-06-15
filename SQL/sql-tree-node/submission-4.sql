ed9dfd5db4dfd00c0eb195355e0246b937989738
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-15 16:59:19 +0530

    Add: sql-tree-node - submission-4

---
+++ SQL/sql-tree-node/submission-4.sql
@@ -0,0 +1,29 @@
+-- Write your query below
+SELECT DISTINCT t2.id, 
+    CASE
+        WHEN t1.p_id IS NULL AND t2.p_id IS NULL THEN 'Root'
+        WHEN t1.p_id IS NOT NULL AND t2.p_id IS NULL THEN 'Root'
+        WHEN t1.p_id IS NOT NULL AND t2.p_id IS NOT NULL THEN 'Inner'
+        WHEN t1.p_id IS NULL AND t2.p_id IS NOT NULL THEN 'Leaf'
+    END AS type
+FROM tree t1
+FULL JOIN tree t2 ON t1.p_id = t2.id
+WHERE t2.id IS NOT NULL
+
+
+-- SELECT id,
+--     CASE
+--         WHEN p_id IS NULL THEN 'Root' 
+--         WHEN id IN (SELECT p_id FROM tree WHERE p_id IS NOT NULL) THEN 'Inner'
+--         ELSE 'Leaf' 
+--     END AS type
+-- FROM tree
+
+
+
+
+
+
+
+
+
