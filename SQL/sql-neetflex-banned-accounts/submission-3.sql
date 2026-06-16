12fdc587cec3a164544695cd1b4221a1ac6c39a0
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-16 12:41:05 +0530

    Add: sql-neetflex-banned-accounts - submission-3

---
+++ SQL/sql-neetflex-banned-accounts/submission-3.sql
@@ -0,0 +1,21 @@
+-- Write your query below
+-- SELECT account_id, 
+--     LAG(logout) OVER(PARTITION BY account_id ORDER BY ip_address) AS prev_logout
+-- FROM log_info
+SELECT account_id
+FROM (
+SELECT 
+    CASE
+        WHEN login <= LAG(logout, 1, '1970-01-01 00:00:00'::timestamp) OVER(PARTITION BY account_id ORDER BY ip_address)
+        AND ip_address != LAG(ip_address, 1, 0) OVER(PARTITION BY account_id ORDER BY ip_address)
+        THEN account_id
+    END AS account_id
+FROM log_info )
+WHERE account_id IS NOT NULL
+-- WHERE account_id IS NOT NULL
+
+
+-- SELECT *,
+--     LAG(logout, 1, '1970-01-01 00:00:00'::timestamp) OVER(PARTITION BY account_id ORDER BY ip_address)
+-- FROM log_info
+
