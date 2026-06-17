9a088ec77ceb11c9adc4655acbabac435d6d96a0
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-17 15:33:10 +0530

    Add: sql-the-most-recent-orders-for-each-product - submission-0

---
+++ SQL/sql-the-most-recent-orders-for-each-product/submission-0.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+
+WITH latest_orders AS (
+    SELECT p.product_id, MAX(o.order_date) as latest_orderdate
+    FROM products p
+    JOIN orders o ON p.product_id = o.product_id
+    GROUP BY p.product_id
+)
+
+SELECT p.product_name, p.product_id, o.order_id, o.order_date
+FROM products p
+JOIN orders o ON p.product_id = o.product_id
+JOIN latest_orders lo ON p.product_id = lo.product_id
+AND o.order_date = lo.latest_orderdate
+ORDER BY product_name ASC, product_id ASC, order_id ASC;
+
