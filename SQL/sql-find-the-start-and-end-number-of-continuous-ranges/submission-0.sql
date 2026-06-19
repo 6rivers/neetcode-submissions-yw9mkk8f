d452e0f726271a9e7f33d5af2822a361f3195089
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-19 13:39:34 +0530

    Add: sql-find-the-start-and-end-number-of-continuous-ranges - submission-0

---
+++ SQL/sql-find-the-start-and-end-number-of-continuous-ranges/submission-0.sql
@@ -0,0 +1,41 @@
+-- Write your query below
+-- SELECT l1.log_id, l2.log_id
+-- FROM logs l1
+-- JOIN logs l2
+-- ON l1.log_id = l2.log_id + 1
+
+WITH intermediate AS (
+    SELECT log_id,
+        LAG(log_id) OVER(ORDER BY log_id ASC) AS prev_logid,
+        LEAD(log_id) OVER(ORDER BY log_id ASC) AS next_logid,
+        (log_id - LAG(log_id) OVER(ORDER BY log_id ASC)) AS diff1,
+        (LEAD(log_id) OVER(ORDER BY log_id ASC) - log_id) AS diff
+    FROM logs
+)
+
+,starters AS (
+    SELECT log_id AS start_id,
+        ROW_NUMBER() OVER (ORDER BY log_id ASC) AS rn
+    FROM intermediate
+    WHERE diff1 IS NULL or diff1 > 1
+),
+enders AS (
+    SELECT log_id AS end_id,
+        ROW_NUMBER() OVER (ORDER BY log_id ASC) AS rn
+    FROM intermediate
+    WHERE diff > 1 or diff IS NULL
+)
+
+SELECT start_id, end_id
+FROM starters
+FULL OUTER JOIN enders ON starters.rn = enders.rn
+
+-- SELECT *
+-- FROM intermediate
+
+-- SELECT
+--     CASE
+--         WHEN prev_logid IS NULL OR diff > 1 OR next_logid IS NULL
+--         THEN log_id
+--     END AS log_id
+-- FROM intermediate
