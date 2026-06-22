b4c2677c0ea2261c27954b1df4119208d267b493
Ranganath <77850060+6rivers@users.noreply.github.com>
2026-06-22 15:18:44 +0530

    Add: sql-the-most-frequently-ordered-products-for-each-customer - submission-1

---
+++ SQL/sql-the-most-frequently-ordered-products-for-each-customer/submission-1.sql
@@ -0,0 +1,25 @@
+-- Write your query below
+WITH customer_total_orders AS (
+    SELECT o.customer_id, o.product_id, p.product_name, 
+    RANK() OVER(PARTITION BY o.customer_id ORDER BY COUNT(*) DESC) AS product_ranking
+    FROM orders o
+    JOIN customers c ON o.customer_id = c.customer_id
+    JOIN products p ON o.product_id = p.product_id
+    GROUP BY o.customer_id, o.product_id, p.product_name
+    -- ORDER BY o.customer_id
+)
+-- ,
+-- order_rankings AS (
+--     SELECT customer_id, product_id, product_name, product_order_count, 
+--     RANK() OVER(PARTITION BY customer_id ORDER BY product_order_count DESC) AS product_ranking
+--     FROM customer_total_orders
+-- )
+
+SELECT customer_id, product_id, product_name
+FROM customer_total_orders
+WHERE product_ranking = 1
+ORDER BY customer_id, product_id
+
+-- SELECT customer_id, product_id, product_name
+-- FROM order_rankings
+-- WHERE product_ranking = 1
